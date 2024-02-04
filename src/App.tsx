df8c5c7c5086b14651b9c739bb10b57b889916d7
snrnapa <shino.satoru@gmail.com>
2024-02-04 23:31:17 +0900

    ファーストコミット

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="text-9xl">hello vite tailwindcss</p>
+    </>
+  );
+}
+
+export default App;
